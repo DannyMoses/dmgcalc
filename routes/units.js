14d1ba506144c999620b78ccbbd7134bc987c915
Daniel Moses <dannymoses555@gmail.com>
2021-04-17 00:10:22 +0000

    finished up basic CRUD for units

--- routes/units.js
+++ routes/units.js
@@ -73,25 +73,74 @@ router.get('/', function (req, res) {
 router.post('/', function(req, res) {
 	console.log(req.body);
 	var keys = Object.keys(req.body);
-	console.log(keys);
+	var badInput = false;
 	keys.forEach(k => {
-		console.log(k);
 		if (properties.indexOf(k) === -1) {
 			console.log("ERROR", k);
 			res.json(notInPropertiesError);
+			badInput = true;
+			return;
+		}
+	})
+	
+	if (badInput) {
+		return;
+	}
+
+	knex('units').insert(req.body).catch(error => {
+		//console.log(error);
+		res.json({"status" : "ERROR" , "message" : "SQL Failure"});
+		return Promise.reject('error');
+	}).then((result) => {
+		// console.log('running then()');
+		// onsole.log(result);
+		res.send({ "status" : "SUCCESS", "inserted" : req.body});
+	}).catch(error => {
+		console.log("One of more errors occured");
+	});
+
+})
+
+router.put('/:name', function(req, res) {
+	console.log("params", req.params);
+	console.log("body", req.body);
+	var badInput = false;
+	var keys = Object.keys(req.body);
+	keys.forEach(k => {
+		if (properties.indexOf(k) === -1) {
+			console.log("ERROR", k);
+			res.json(notInPropertiesError);
+			badInput = true;
 			return;
 		}
 	})
 
-	knex('units').insert({name: req.body['name'], health : req.body['health'], 'class' : req.body['class'], 'dexterity' : req.body['dexterity'] }).then((result) => {
-		console.log(result);
-	});
+	if(badInput) {
+		return;
+	}
 
-	res.send({ "status" : "SUCCESS", "inserted" : req.body});
+	var query = {};
+	query['name'] = req.params['name'];
+	keys.forEach(k => {
+		query[k] = req.body[k];
+	})
+	console.log('query', query);
+	
+	knex('units').where({name : req.params['name']}).update(query).then((result) => {
+		console.log(result);
+		res.send({"status" : "SUCCESS"});
+	}).catch(error => {
+		res.send({"status" : "ERROR", "message" : "an error occurred"});
+	})
+
 })
 
-
-
-
+router.delete('/:name', function(req, res) {
+	knex('units').where({name : req.params['name']}).del().then((result) => {
+		res.send({"status" : "SUCCESS"});
+	}).catch(error => {
+		res.send({"status" : "ERROR", "message" : "an error occurred"});
+	});
+})
 
 module.exports = router;
